fa52d8ef957dacb0cf5daa12035522740b627fc6
seymanurks <nurks1905@gmail.com>
2024-02-12 21:58:24 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, SafeAreaView, Text} from "react-native";
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <View>
+         <Text>Hello World!</Text>
+       </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
